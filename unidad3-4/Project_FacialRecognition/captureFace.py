5bbec0cb2d29a619ffdea7866f2c700e1de74f30
Cronando <lurear11@hotmail.com>
2024-05-24 23:48:41 -0700

    Advancements of Facial Recognition Project

--- unidad3-4/Project_FacialRecognition/captureFace.py
+++ unidad3-4/Project_FacialRecognition/captureFace.py
@@ -3,22 +3,26 @@ import os
 import imutils
 
 personName = 'Luis'
-dataPath = '/data'
-personPath = dataPath + '/' + personName
+dataPath = './data'
+personPath = os.path.join(dataPath, personName)
 print(personPath)
 
+# Create directory if it doesn't exist
 if not os.path.exists(personPath):
     print('Carpeta creada: ', personPath)
     os.makedirs(personPath)
 
-cap = cv2.VideoCapture('Luis.mp4')
+# Capture video
+cap = cv2.VideoCapture('./videos/Luis.mp4')
 
+# Load Haar cascade classifier for face detection
 faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 count = 0
 
 while True:
     ret, frame = cap.read()
-    if ret == False: break
+    if not ret:
+        break
     frame = imutils.resize(frame, width=640)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     auxFrame = frame.copy()
@@ -26,11 +30,12 @@ while True:
     faces = faceClassif.detectMultiScale(gray, 1.3, 5)
 
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, +h), (0, 255, 0), 2)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
         face = auxFrame[y:y+h, x:x+w]
         face = cv2.resize(face, (150, 150), interpolation=cv2.INTER_CUBIC)
-        cv2.imwrite(personPath + '/myFace_{}.jpg'.format(count), face)
-        count = count + 1
+        cv2.imwrite(os.path.join(personPath, f'myFace_{count}.jpg'), face)
+        count += 1
+
     cv2.imshow('frame', frame)
 
     k = cv2.waitKey(1)
