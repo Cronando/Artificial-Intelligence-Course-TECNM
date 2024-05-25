8b750cfc742547cd581684b8dbc303ead8ea56ab
Luis Orlando <lurear11@hotmail.com>
2024-05-25 13:57:16 -0700

    Almosted finished Facial Recognition Project

--- unidad3-4/Project_FacialRecognition/trainingFR.py
+++ unidad3-4/Project_FacialRecognition/trainingFR.py
@@ -19,13 +19,13 @@ for nameDir in peopleList:
         print('Faces: ', nameDir + '/' + fileName)
         labels.append(label)
         facesData.append(cv2.imread(personPath + '/' + fileName, 0))
-        #   image = cv2.imread(personPath + '/' + fileName, 0)
-        #   cv2.imshow('image', image)
-        #   cv2.waitKey(10)
+        image = cv2.imread(personPath + '/' + fileName, 0)
+        cv2.imshow('image', image)
+        cv2.waitKey(10)
     label = label + 1
 
-#   print('Labels: ', labels)
-#   print('Number of labels 0: ', np.count_nonzero(np.array(labels) == 0))
+print('Labels: ', labels)
+print('Number of labels 0: ', np.count_nonzero(np.array(labels) == 0))
 
 face_recognizer = cv2.face.EigenFaceRecognizer_create()
 
@@ -38,3 +38,4 @@ if not os.path.exists(efmPath):
     os.makedirs(efmPath)
 
 face_recognizer.write(efmPath + '/' + 'EigenFaceModel.xml')
+print('Saving model...')
